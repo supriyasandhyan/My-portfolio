8981b9ea00436e1b89fa109887ea0f51ae55e6c3
Supriya Premraj Sandhyan <47947363+supriyasandhyan@users.noreply.github.com>
2024-04-30 16:55:55 +0530

    Create TechstackList.js

---
+++ src/utils/TechstackList.js
@@ -0,0 +1,67 @@
+import { FaHtml5 , FaCss3, FaReact, FaBootstrap, FaNodeJs} from "react-icons/fa";
+import { SiMui, SiMysql, SiExpress, SiMongodb , SiFirebase } from "react-icons/si";
+import { TbBrandJavascript, TbBrandRedux } from "react-icons/tb";
+
+
+export const TechstackList = [
+    {
+        _id : 1,
+        name:"HTML",
+        icon: FaHtml5,
+    },
+    {
+        _id : 2,
+        name:"CSS",
+        icon: FaCss3,
+    },
+    {
+        _id : 3,
+        name:"JavaScript",
+        icon: TbBrandJavascript,
+    },
+    {
+        _id : 4,
+        name:"React JS",
+        icon: FaReact,
+    },
+    {
+        _id : 5,
+        name:"Bootstrap",
+        icon: FaBootstrap,
+    },
+    {
+        _id : 6,
+        name:"React MUI",
+        icon: SiMui,
+    },
+    {
+        _id : 7,
+        name:"Redux",
+        icon: TbBrandRedux,
+    },
+    {
+        _id : 8,
+        name:"SQL",
+        icon: SiMysql,
+    },
+    {
+        _id : 9,
+        name:"Firebase",
+        icon: SiFirebase,
+    },
+    {
+        _id : 10,
+        name:"Mongodb",
+        icon: SiMongodb,
+    },
+    {
+        _id : 11,
+        name:"Node JS",
+        icon: FaNodeJs,
+    },
+    {
+        _id : 9,
+        name:"Express JS",
+        icon: SiExpress,
+    },
+]
